2f23ce365cd7a3d1e198e49229e480649f91dc28
michi <michiadam@protonmail.com>
2022-04-01 23:27:29 +0200

    Fixed Bug for multiple module generation.

--- src/main/java/at/michaeladam/controller/BaseMojo.java
+++ src/main/java/at/michaeladam/controller/BaseMojo.java
@@ -13,10 +13,29 @@ public abstract class BaseMojo extends AbstractMojo {
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
     protected MavenProject project;
 
+    @Parameter(defaultValue = "${project.build.directory}", required = true)
+    private File buildDirectory;
+
     @Parameter(defaultValue = "${project.build.directory}/classTree.yaml", required = true)
     protected File outputDirectory;
 
     @Parameter(required = false)
     protected File targetFolder;
+
+    public File getTargetFolder() {
+        if (targetFolder == null) {
+            if(!buildDirectory.exists()) {
+                if(buildDirectory.mkdirs()){
+                    getLog().info("Created build directory: " + buildDirectory.getAbsolutePath());
+                } else {
+                    getLog().error("Could not create build directory: " + buildDirectory.getAbsolutePath());
+                }
+            }
+            return outputDirectory;
+
+        }
+
+        return targetFolder;
+    }
 
 }
